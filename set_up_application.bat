90e44540309f89d82177b9073dfd119db52611bd
Akshay2002Singh <06akshay2002@gmail.com>
2023-09-14 19:19:41 +0530

    delete files that are not needed on using setup_application.bat

--- set_up_application.bat
+++ set_up_application.bat
@@ -18,4 +18,9 @@ echo "Installing requirements..."
 echo.
 pip install -r Requirements.txt
 
+echo "Deleting Files that are not required..."
+echo.
+rmdir /s /q "Important_Codes"
+del README.md .gitignore set_up_application.bat Requirements.txt
+
 timeout /t 15
